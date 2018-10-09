4eafb16828dfcd71b8158bc021b9d4e248cb0762
vk-webdesign <42648435+vk-webdesign@users.noreply.github.com>
2018-10-09 15:14:40 -0700

    create vertical-lines.js

---
+++ hw5/vertical-lines.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  
+  for (var i = 20; i <= 380; i = i + 10) {
+  	var startX = i;
+  	var startY = 20;
+  	var endX = i;
+  	var endY = 380;
+  	line(startX, startY, endX, endY);
+	}
+}
